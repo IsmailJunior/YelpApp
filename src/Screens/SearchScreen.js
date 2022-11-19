6568a3e31b288c5ba21cba3626f5756794a94f57
Merced <77894409+ismailMerced@users.noreply.github.com>
2022-11-19 20:01:19 +0300

    init

---
+++ src/Screens/SearchScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreen = () =>
+{
+	return (
+		<View>
+			<Text>Hello</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create( {
+	text: {
+		fontSize: 40
+	}
+} );
+
+export default SearchScreen;
